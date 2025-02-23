2a47a3acc67d48c24ca65a7640cc63dc6459f46f
holim0 <holim1226@gmail.com>
2025-02-23 16:13:42 +0900

    test: 상품 리스트 테스트 코드 작성

--- src/pages/home/components/tests/ProductList.spec.jsx
+++ src/pages/home/components/tests/ProductList.spec.jsx
@@ -1,4 +1,5 @@
 import { screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import data from '@/__mocks__/response/products.json';
@@ -27,11 +28,63 @@ vi.mock('react-router-dom', async () => {
   };
 });
 
-it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {});
+it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {
+  await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
 
-it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {});
+  const productList = await screen.findAllByTestId('product-card');
+  expect(productList).toHaveLength(PRODUCT_PAGE_LIMIT);
 
-it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {});
+  productList.forEach((product, index) => {
+    const productCard = within(product);
+    const productData = data.products[index];
+    expect(productCard.getByText(productData.title)).toBeInTheDocument();
+
+    expect(
+      productCard.getByText(productData.category.name),
+    ).toBeInTheDocument();
+
+    expect(
+      productCard.getByText(formatPrice(productData.price)),
+    ).toBeInTheDocument();
+
+    expect(
+      productCard.getByRole('button', { name: '장바구니' }),
+    ).toBeInTheDocument();
+
+    expect(
+      productCard.getByRole('button', { name: '구매' }),
+    ).toBeInTheDocument();
+  });
+});
+
+it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {
+  await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+  const user = userEvent.setup();
+  await screen.findAllByTestId('product-card');
+
+  expect(
+    screen.getByRole('button', {
+      name: 'Show more',
+    }),
+  ).toBeInTheDocument();
+
+  await user.click(screen.getByRole('button', { name: 'Show more' }));
+
+  const newProductList = await screen.findAllByTestId('product-card');
+  expect(newProductList).toHaveLength(PRODUCT_PAGE_LIMIT * 2);
+});
+
+it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {
+  await render(<ProductList limit={50} />);
+
+  await screen.findAllByTestId('product-card');
+
+  expect(
+    screen.queryByRole('button', {
+      name: 'Show more',
+    }),
+  ).not.toBeInTheDocument();
+});
 
 describe('로그인 상태일 경우', () => {
   beforeEach(() => {
@@ -39,87 +92,86 @@ describe('로그인 상태일 경우', () => {
   });
 
   it('구매 버튼 클릭시 addCartItem 메서드가 호출되며, "/cart" 경로로 navigate 함수가 호출된다.', async () => {
+    await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+    const user = userEvent.setup();
     const addCartItemFn = vi.fn();
     mockUseCartStore({ addCartItem: addCartItemFn });
-
-    const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
-
     await screen.findAllByTestId('product-card');
 
-    // 첫번째 상품을 대상으로 검증한다.
     const productIndex = 0;
     await user.click(
       screen.getAllByRole('button', { name: '구매' })[productIndex],
     );
 
-    expect(addCartItemFn).toHaveBeenNthCalledWith(
-      1,
+    expect(addCartItemFn).toHaveBeenCalledWith(
       data.products[productIndex],
       10,
       1,
     );
-    expect(navigateFn).toHaveBeenNthCalledWith(1, '/cart');
+    expect(navigateFn).toHaveBeenCalledWith('/cart');
   });
 
   it('장바구니 버튼 클릭시 "장바구니 추가 완료!" toast를 노출하며, addCartItem 메서드가 호출된다.', async () => {
+    await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+    const user = userEvent.setup();
     const addCartItemFn = vi.fn();
     mockUseCartStore({ addCartItem: addCartItemFn });
-
-    const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
-
     await screen.findAllByTestId('product-card');
 
-    // 첫번째 상품을 대상으로 검증한다.
     const productIndex = 0;
-    const product = data.products[productIndex];
     await user.click(
       screen.getAllByRole('button', { name: '장바구니' })[productIndex],
     );
 
-    expect(addCartItemFn).toHaveBeenNthCalledWith(1, product, 10, 1);
+    expect(addCartItemFn).toHaveBeenCalledWith(
+      data.products[productIndex],
+      10,
+      1,
+    );
     expect(
-      screen.getByText(`${product.title} 장바구니 추가 완료!`),
+      screen.getByText(
+        `${data.products[productIndex].title} 장바구니 추가 완료!`,
+      ),
     ).toBeInTheDocument();
   });
 });
 
 describe('로그인이 되어 있지 않은 경우', () => {
   it('구매 버튼 클릭시 "/login" 경로로 navigate 함수가 호출된다.', async () => {
-    const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
-
+    await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+    const user = userEvent.setup();
     await screen.findAllByTestId('product-card');
 
-    // 첫번째 상품을 대상으로 검증한다.
     const productIndex = 0;
     await user.click(
       screen.getAllByRole('button', { name: '구매' })[productIndex],
     );
 
-    expect(navigateFn).toHaveBeenNthCalledWith(1, '/login');
+    expect(navigateFn).toHaveBeenCalledWith('/login');
   });
 
   it('장바구니 버튼 클릭시 "/login" 경로로 navigate 함수가 호출된다.', async () => {
-    const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
-
+    await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+    const user = userEvent.setup();
     await screen.findAllByTestId('product-card');
 
-    // 첫번째 상품을 대상으로 검증한다.
     const productIndex = 0;
     await user.click(
       screen.getAllByRole('button', { name: '장바구니' })[productIndex],
     );
 
-    expect(navigateFn).toHaveBeenNthCalledWith(1, '/login');
+    expect(navigateFn).toHaveBeenCalledWith('/login');
   });
 });
 
 it('상품 클릭시 "/product/:productId" 경로로 navigate 함수가 호출된다.', async () => {
-  const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+  await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+  const user = userEvent.setup();
+  await screen.findAllByTestId('product-card');
 
-  const [firstProduct] = await screen.findAllByTestId('product-card');
-
-  // 첫번째 상품을 대상으로 검증한다.
-  await user.click(firstProduct);
-
-  expect(navigateFn).toHaveBeenNthCalledWith(1, '/product/6');
+  const productIndex = 0;
+  await user.click(screen.getAllByTestId('product-card')[productIndex]);
+  expect(navigateFn).toHaveBeenCalledWith(
+    `/product/${data.products[productIndex].id}`,
+  );
 });
